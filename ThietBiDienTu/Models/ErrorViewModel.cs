e07c4a5901188cff80a300c25f7ea44caa380804
huannguyen310 <huanchappie0809@gmail.com>
2023-09-26 14:53:35 +0700

    Create webApp

---
+++ ThietBiDienTu/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ThietBiDienTu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
