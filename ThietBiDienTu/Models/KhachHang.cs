0b881503b8ecb9bb5079912be72161947ec73d84
huannguyen310 <huanchappie0809@gmail.com>
2023-11-27 16:56:06 +0700

    Them thuoc tinh cho khach hang

---
+++ ThietBiDienTu/Models/KhachHang.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ThietBiDienTu.Models
+{
+    public class KhachHang
+    {
+        [Key]
+        [Required(ErrorMessage = "Mã khách hàng không được để trống!")]
+        public string MaKH { get; set; }
+        [Required(ErrorMessage = "Tên khách hàng không được để trống!")]
+        public string TenKH { get; set; }
+        public string DiaChi { get; set; }
+        public int SDT { get; set; }
+    }
+}
