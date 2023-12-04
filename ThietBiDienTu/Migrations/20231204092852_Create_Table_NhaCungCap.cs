9181b71ebdbc60c6dd2435624bf86503f8f672f6
huannguyen310 <huanchappie0809@gmail.com>
2023-12-04 16:30:35 +0700

    Ket noi CSDL class NhaCungCap

---
+++ ThietBiDienTu/Migrations/20231204092852_Create_Table_NhaCungCap.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ThietBiDienTu.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_Table_NhaCungCap : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NhaCungCap",
+                columns: table => new
+                {
+                    MaNCC = table.Column<string>(type: "TEXT", nullable: false),
+                    TenNCC = table.Column<string>(type: "TEXT", nullable: false),
+                    DiaChi = table.Column<string>(type: "TEXT", nullable: false),
+                    SDT = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NhaCungCap", x => x.MaNCC);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NhaCungCap");
+        }
+    }
+}
